f658dfb82b18c09e864fc1ca2d7da92aeea650c7
610yilingliu <yilingliu1994@gmail.com>
2020-12-20 13:24:21 +0800

    auto update update_time

---
+++ autoupdater.py
@@ -0,0 +1,22 @@
+import subprocess
+import time
+
+def time_helper(seperator = '_', to_sec = False):
+    """
+    return a string like 2020_09_11_22_43_00 (if to_sec is True) or 2020_09_11_22_43 (if to_sec is False)
+    """
+    localtime = time.asctime(time.localtime(time.time()))
+    if to_sec:
+        return time.strftime("%Y" + seperator + "%m" + seperator + "%d" + seperator + "%H" + seperator + "%M" + seperator + "%S", time.localtime()) 
+    return time.strftime("%Y" + seperator + "%m" + seperator + "%d" + seperator + "%H" + seperator + "%M", time.localtime()) 
+
+add_git = "git add ."
+update_time = time_helper('-')
+commit_git = "git commit -m \" auto update " + "update_time\""
+push_git = "git push origin main"
+
+while True:
+    subprocess.call(add_git)
+    subprocess.call(commit_git)
+    subprocess.call(push_git)
+    time.sleep(3600)
